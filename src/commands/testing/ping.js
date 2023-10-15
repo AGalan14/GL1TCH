8b62a41f9f2650c10cbb7f959706a3262d028fea
AGalan14 <angelgalanaixut14@gmail.com>
2023-10-15 23:32:07 +0200

    Ready and interactionCreate events

--- src/commands/testing/ping.js
+++ src/commands/testing/ping.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+    cooldown: 20,
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Replies with Pong!')
+        .setDescriptionLocalizations({
+            "es-ES": 'Contesta con "Hola!"'
+        }),
     async execute(interaction) {
-        await interaction.reply('Pong!');
+        const locales = { "es-ES": 'Hola!' };
+        
+        await interaction.reply(locales[interaction.locale] ?? 'Pong!');
     }
 };
---
+++ src/events/ready.js
@@ -0,0 +1,29 @@
+const { Events, ActivityType } = require('discord.js');
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        console.log(`${client.user.tag} is ready, folks!`);
+
+        const activities = [
+            { name: `What are you looking at..? ಠ_ಠ`, type: ActivityType.Custom },
+            { name: `🛏️ ZzZzz...`, type: ActivityType.Custom },
+            { name: `Greetings, folks! ^-^`, type: ActivityType.Custom }
+        ];
+
+        const status = [
+            'dnd',
+            'idle',
+            'online'
+        ];
+
+        let i = 0;
+        setInterval(() => {
+            if(i >= activities.length) i=0;
+            client.user.setActivity(activities[i]);
+            client.user.setStatus(status[i]);
+            i++;
+        }, 5000);
+    }
+};
